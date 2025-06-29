da16ecd2c35aaca3df6fe5f22f1ed75dc5c16bfb
PhongVuong <vkphong23@clc.fitus.edu.vn>
2025-06-30 01:21:14 +0700

    Don't insult Quang anymore

--- Source/helpFunctions.py
+++ Source/helpFunctions.py
@@ -1,7 +1,3 @@
-# File để chứa những hàm hỗ trợ trong quá trình code các thuật toán search ví dụ như hàm tìm Heuristic cho A*
-# anh em mình cố gắng code bằng tiếng anh và cmt bằng tiếng anh luôn nha
-# code tới đâu cmt tới đó nha, không cần quá chi tiết cũng được
-# nhớ là phải code sạch nha Quang, cứ sau một vòng lặp hoặc một điều kiện là phải cách dòng nha Quang
 import json
 from vehicle import Vehicle
 from gameboard import Gameboard
--- Source/searchAlgorithms.py
+++ Source/searchAlgorithms.py
@@ -7,16 +7,6 @@ import psutil
 import os
 from collections import deque
 
-# File để chứa toàn bộ 4 thuật toán search
-# anh em mình cố gắng code bằng tiếng anh và cmt bằng tiếng anh luôn nha
-# code tới đâu cmt tới đó nha, không cần quá chi tiết cũng được
-# nhớ là phải code sạch nha Quang :)
-# nhớ là phải code sạch nha Quang, cứ sau một vòng lặp hoặc một điều kiện là phải cách dòng nha Quang
-
-# Wang Qi Feng, you brat, how dare you insult me :((
-
-
-
 # IDS algorithm
 
 # BFS algorithm
