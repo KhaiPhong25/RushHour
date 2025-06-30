5293fb7d4e184075f223d86e850d297142de639c
Nguyen An <nguyenvanan2511205@gmail.com>
2025-06-30 23:33:32 +0700

    update BFS 2

--- Source/searchAlgorithms.py
+++ Source/searchAlgorithms.py
@@ -1,73 +1,88 @@
 from gameboard import Gameboard
-import helpFunctions
 import time
-import resource
 from queue import PriorityQueue
 import psutil
 import os
 from collections import deque
+import tracemalloc
+from helpFunctions import load_gameboard
 from vehicle import Vehicle
 
 # IDS algorithm
 
 # BFS algorithm
 def bfs_algorithm(gameboard: Gameboard):
-    # Start calculating the time
+    #Start memory tracing
+    tracemalloc.start()
+
+    # Get start time currently
     start = time.time()
-    
+
     # Number of expanded nodes
     expanded_nodes = 0
-    
     # Initialize the queue for BFS
     queue = deque()
-    
     # Keep track of visited states to avoid cycles
     visited = set()
 
     # Start with the initial state of the gameboard
-    start_state = gameboard.get_state()
+    #start_state = gameboard.get_state()
 
-    # If the initial state is already solved, return the solution path
-    if gameboard.hasSolved(start_state):
+    # If the start state is already solved, return the solution path
+    if gameboard.hasSolved():
         end = time.time()
-        peak_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0
-        
+        current, peak = tracemalloc.get_traced_memory()
         print(f'Total runtime of the solution is {end - start} seconds')
-        print(f'Peak memory usage is {peak_memory_usage} megabytes')
+        print(f'Peak memory usage is {peak / (1024 * 1024)} megabytes')
         print(f'Total expanded nodes is {expanded_nodes} nodes')
-        
-        return gameboard.get_solution_path(start_state)
+        tracemalloc.stop()
+        return gameboard.get_solution_path()
+    
+    # Add the start state to the queue and mark it as visited
+    queue.append(gameboard)
+    visited.add(gameboard)
 
-    # Initialize the queue with the start state
-    queue.append(start_state)
-    visited.add(start_state)
-
-    # While there are states in the queue, continue searching
+    # While there are states to explore in the queue
     while queue:
-        # Get the current state from the queue
-        current_state = queue.popleft()
-        # Incresing the number of expanded nodes
+        # Get the current state from the queue and increase the expanded nodes count
+        current_gameboard = queue.popleft()
         expanded_nodes += 1
-        # Generate successors for the current state
-        for next_state in gameboard.get_successors(current_state):
-            # If the new state has not been visited, add it to the queue, mark it as visited and check if it is the goal state
-            if next_state not in visited:
-                # If the new state is the goal state, return the solution path
-                if gameboard.hasSolved(next_state):
-                    end = time.time()
-                    peak_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0
-                    
-                    print(f'Total runtime of the solution is {end - start} seconds')
-                    print(f'Peak memory usage is {peak_memory_usage} megabytes')
-                    print(f'Total expanded nodes is {expanded_nodes} nodes')
-                    
-                    return gameboard.get_solution_path(next_state)
-            
-                queue.append(next_state)
-                visited.add(next_state)
 
-    # If no solution is found, return None
+        # Get the successors of the current state
+        for child in current_gameboard.checkformoves():
+            next_gameboard = Gameboard(gameboard.width, gameboard.height, child)
+            # If the next state has not been visited yet
+            if next_gameboard.get_state() not in visited:
+                # Check if the next state has solved the game
+                if next_gameboard.hasSolved():
+                    end = time.time()
+                    current, peak = tracemalloc.get_traced_memory()
+                    print(f'Total runtime of the solution is {end - start} seconds')
+                    print(f'Peak memory usage is {peak / (1024 * 1024)} megabytes')
+                    print(f'Total expanded nodes is {expanded_nodes} nodes')
+                    tracemalloc.stop()
+                    return next_gameboard.get_state()
+                
+                # If not solved, add the next state to the queue and mark it as visited
+                queue.append(next_gameboard)
+                visited.add(next_gameboard)
+
+    # If no solution is found, return None and print statistics
+    end = time.time()
+    current, peak = tracemalloc.get_traced_memory()
+    print(f'Total runtime of the solution is {end - start} seconds')
+    print(f'Peak memory usage is {peak / (1024 * 1024)} megabytes')
+    print(f'Total expanded nodes is {expanded_nodes} nodes')
+    tracemalloc.stop()
+    
     return None
+
+# In ra để check Gameboard
+filename = ".//Map//gameboard1.json"
+gameboard = load_gameboard(filename)
+print(gameboard)
+print (gameboard.vehicles)
+print(bfs_algorithm(gameboard))
 
 
 # UCS algorithm
