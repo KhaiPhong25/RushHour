49babfce348d630457e3bb05a1bb0c5aa141f151
vnquy126904 <ngocquyvan2005@gmail.com>
2025-06-30 14:02:10 +0700

    add heuristic function (count the number of blocking vehicle)

--- Source/helpFunctions.py
+++ Source/helpFunctions.py
@@ -23,7 +23,43 @@ def load_gameboard(file_name):
     # Return the Gameboard object
     return gameboard
 
+# heuristic function for A* algorithm
+def number_blocking_vehicle(game_board):
+    """
+    Counts the number of distinct vehicles blocking the target vehicle's path to the exit.
+    
+    Input:
+        game_board: The current game board state containing vehicles and layout.
+        
+    Output:
+        int: Number of unique vehicles blocking the target vehicle's right path.
+    """
+    result = 0
+    vehicles = game_board.vehicles
+
+    # The target vehicle is assumed to be the first in the list (typically the red car)
+    target_vehicle = vehicles[0]
+
+    # Get target vehicle's rightmost position 
+    y_position = target_vehicle.y
+    x_position = target_vehicle.x + target_vehicle.length - 1
+
+    # Track last seen vehicle to avoid double-counting the same vehicle
+    last_character = '.' # '.' represents empty space
+
+    # Scan all cells to the right of the target vehicle
+    for col in range(x_position + 1, game_board.width):
+        current_cell = game_board.board[y_position][col]
+        # If we find a new vehicle (not empty and different from last seen)
+        if current_cell != '.' and current_cell != last_character:
+            result += 1
+            last_character = current_cell # Remember this vehicle to avoid recounting
+    
+    return result
+
 # In ra để check Gameboard
-filename = "gameboard1.json"
+filename = 'gameboard1.json'
 gameboard = load_gameboard(filename)
 print(gameboard)
+print (gameboard.vehicles)
+print(number_blocking_vehicle(gameboard))
